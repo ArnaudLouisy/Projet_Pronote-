a461c18b2e5bf7ba4ed2eb99ca0e0f8bf1665c82
ArnaudLouisy <louisy.arnaud@gmail.com>
2022-03-22 16:46:09 +0100

    feat : ajout choix connexion + classe prof et direction + connexion eleves front and back end

---
+++ src/traitement/connexionEleves.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require_once 'src/bdd/BDD.php';
+$bdd = new Bdd();
+
+$req = $bdd->getBdd()->prepare('SELECT * FROM eleves WHERE mail = :mail AND mot_de_passe = :mot_de_passe');
+$req->execute(array(
+    'mail' => $_POST['mail'],
+    'mot_de_passe' => $_POST['mot_de_passe'],
+));
+
+$res = $req->fetch();
+
+if ($res) {
+    $_SESSION['mail'] = $res['mail'];
+    $_SESSION['id_eleves'] = $res['id_eleves'];
+
+    header('Location:../index.html');
+}
+else{
+    header('Location: connexionEleves.html');
+}
+
---
+++ src/traitement/typeconnexion.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Muhamad Nauval Azhar">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="This is a login page template based on Bootstrap 5">
+    <title>Panel Administration</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+<div class="align-items-center d-flex">
+</div>
+</form>
+</div>
+<div class="card-footer py-3 border-0">
+    <div class="text-center">
+        Ce connecter en tant que Eleves<br> <a class="btn btn-secondary" href="connexionEleves.html" role="button">Connexion Eleves </a> <br><br>
+        Ce connecter en tant que Professeur <br> <a class="btn btn-secondary" href="connexionProf.html" role="button">Connexion Professeur</a>
+        <br>
+        <br>
+        Voir la liste des films ? <br> <a class="btn btn-secondary" href="listfilm.php" role="button">Films</a> <br> <br>
+
+    </div>
+</div>
+</div>
+<div class="text-center mt-5 text-muted">
+    Copyright &copy; 2022 &mdash; Intranet Louisy
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
